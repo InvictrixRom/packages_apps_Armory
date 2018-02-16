6ac736668fa97ec8833df7979ec6e4752ab70067
Jacob McSwain <jacob.a.mcswain@gmail.com>
2018-02-16 17:38:43 -0600

    Add statusbar dt2s
    Change-Id: I0a8621756abe4d2bbedb81fab210564fc8f1e41b

--- src/com/armory/settings/ui/StatusBarSettings.java
+++ src/com/armory/settings/ui/StatusBarSettings.java
@@ -22,16 +22,37 @@ import android.os.UserHandle;
 import android.provider.Settings;
 
 import android.support.v7.preference.Preference;
+import android.support.v14.preference.SwitchPreference;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 
 import com.android.settings.R;
 
-public class StatusBarSettings extends InvictrixSettingsFragment {
+public class StatusBarSettings extends InvictrixSettingsFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    SwitchPreference mDoubleTapToSleepEnabled;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         title = getResources().getString(R.string.statusbar_settings_title);
         addPreferencesFromResource(R.xml.settings_statusbar);
+
+        mDoubleTapToSleepEnabled = (SwitchPreference) findPreference("double_tap_sleep_gesture");
+        mDoubleTapToSleepEnabled.setChecked(Settings.System.getInt(getContentResolver(),
+                Settings.System.DOUBLE_TAP_SLEEP_GESTURE, 0) == 1);
+        mDoubleTapToSleepEnabled.setOnPreferenceChangeListener(this);
+
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference.equals(mDoubleTapToSleepEnabled)) {
+            boolean enabled = ((Boolean) newValue).booleanValue();
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.DOUBLE_TAP_SLEEP_GESTURE, enabled ? 1 : 0);
+            return true;
+        }
+        return false;
     }
 }
