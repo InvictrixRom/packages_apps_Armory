da3422d57b3b5c9f65d1989999de8d7a8606a77b
Jacob McSwain <jacob.a.mcswain@gmail.com>
2018-02-16 17:44:21 -0600

    SystemUI: Double tap to sleep anywhere on the lockscreen (2/2)
    Change-Id: I3000020da7294da866d5b63b21c2d9ce535c696d

--- src/com/armory/settings/ui/LockScreenSettings.java
+++ src/com/armory/settings/ui/LockScreenSettings.java
@@ -16,19 +16,43 @@
 
 package com.armory.settings.ui;
 
+import android.content.ContentResolver;
 import android.os.Bundle;
+import android.os.UserHandle;
+import android.provider.Settings;
 
 import android.support.v7.preference.Preference;
+import android.support.v14.preference.SwitchPreference;
 import android.support.v7.preference.Preference.OnPreferenceChangeListener;
 
 import com.android.settings.R;
 
-public class LockScreenSettings extends InvictrixSettingsFragment {
+public class LockScreenSettings extends InvictrixSettingsFragment implements
+        Preference.OnPreferenceChangeListener {
 
+	SwitchPreference mDoubleTapToSleepAnywhere;
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         title = getResources().getString(R.string.lockscreen_settings_title);
         addPreferencesFromResource(R.xml.settings_lockscreen);
+		
+        mDoubleTapToSleepAnywhere = (SwitchPreference) findPreference("double_tap_sleep_anywhere");
+        mDoubleTapToSleepAnywhere.setChecked(Settings.System.getInt(getContentResolver(),
+                Settings.System.DOUBLE_TAP_SLEEP_ANYWHERE, 0) == 1);
+        mDoubleTapToSleepAnywhere.setOnPreferenceChangeListener(this);
+
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (preference.equals(mDoubleTapToSleepAnywhere)) {
+            boolean enabled = ((Boolean) newValue).booleanValue();
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.DOUBLE_TAP_SLEEP_ANYWHERE, enabled ? 1 : 0);
+            return true;
+        }
+        return false;
     }
 }
